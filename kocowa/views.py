378cf98cbc292031320a05ad872256fd48e690be
lavamatchoco <38874351+lavamatchoco@users.noreply.github.com>
2021-03-03 15:42:55 +0900

    add join_weekly_membership service
    add join_weekly_membership service & update plan.html

--- kocowa/views.py
+++ kocowa/views.py
@@ -5,7 +5,8 @@ from userauth.models import CustomUser
 from userauth.admin import UserCreationForm
 from django.views.decorators.csrf import csrf_exempt
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from membership.models import CustomUserMembership, Subscription
 
 class UserCreateView(CreateView):
     template_name = 'registration/register.html'
@@ -29,7 +30,6 @@ def mykocowa(request):
         return render(request, 'registration/login.html')
     
 def plan(request):
-    print(type(request))
     return render(request,'plan.html')
 
 def get_client_ip(request):
@@ -40,3 +40,32 @@ def get_client_ip(request):
         ip = request.META.get('REMOTE_ADDR')
     return HttpResponse(ip ,content_type='text/plain')
 
+@csrf_exempt
+def join_weekly_membership(request):
+    if not request.user.is_authenticated:
+        return render(request,"login.html")
+    user = request.user
+    memlist = CustomUserMembership.objects.filter(customuser_id_id = user.id).order_by('-id')[:1]
+    if not memlist:
+        #membership 가입이 처음인 회원
+        addMember = CustomUserMembership.objects.create(period_day=7,customuser_id_id=user.id)
+        addMember.save()
+        addSub = Subscription.objects.create(active=True,user_membership_id=addMember.id)
+        addSub.save()
+    else:
+        #membership에 가입한 적이 있는 회원
+        #구독 중인지 확인하기
+        subscript = Subscription.objects.get(user_membership_id = memlist[0].id)
+        if not subscript:
+            #membershiplist에는 있는데 구독이 끝난 회원
+            addMember = CustomUserMembership.objects.create(period_day=7, customuser_id_id= user.id)
+            addMember.save()
+            addSub = Subscription.objects.create(active=True, user_membership_id=addMember.id)
+            addSub.save()
+        else:
+            #구독 중인 회원
+            context = {'result_msg': "WEEKLY MEMBERSHIP 이미 구독 하였습니다!"}
+            return JsonResponse(context,content_type="application/json")
+
+    context = {'result_msg':" WEEKLY MEMBERSHIP 구독 신청하였습니다!"}
+    return JsonResponse(context,content_type="application/json")
