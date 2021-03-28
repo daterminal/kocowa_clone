704fb04590f61e03202ef3362e890d4ed0a2d55b
Doyeon <eded0902@naver.com>
2021-03-28 10:00:08 +0900

    chart2(Number of Users) 완성

--- chart/urls.py
+++ chart/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('chart1/', views.chart, name='chart'),
     path('chart2/', views.chart2, name="chart2"),
+    path('chart2/bySex/', views.chart2bySex, name="chart2bySex"),
+    path('chart2/byAge/', views.chart2byAge, name="chart2byAge"),
+    path('chart2/byCity/', views.chart2byCity, name="chart2byCity")
 ]
--- chart/views.py
+++ chart/views.py
@@ -77,9 +77,193 @@ def chart(request):
     #         elif rsDuration[i][j]:
     #             value[j] += 1
 
-    return render(request, "chart.html", {
+    return render(request, "chart/chart.html", {
         'rsDuration': value
     })
 
 def chart2(request):
-    return render(request, "chart2.html", {})
+    import pymysql
+    dbCon = pymysql.connect(host='223.194.46.212',
+                            user='root',
+                            password='12345!',
+                            db='kocowa')
+    cursor = dbCon.cursor()
+
+    with dbCon:
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct weblog.member_no)
+        FROM weblog RIGHT OUTER JOIN time ON (time.hour=HOUR(weblog.log_date))
+        GROUP BY hour
+        """)
+        rsMemberUseByTime = cursor.fetchall()
+
+    return render(request, "chart/chart2.html", {
+        'rsMemberUseByTime':rsMemberUseByTime
+    })
+
+def chart2bySex(request):
+    import pymysql
+    dbCon = pymysql.connect(host='223.194.46.212',
+                            user='root',
+                            password='12345!',
+                            db='kocowa')
+    cursor = dbCon.cursor()
+
+    with dbCon:
+        # 여자
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct A.mno)
+        from(SELECT weblog.member_no mno, weblog.log_date log_date
+        FROM weblog LEFT OUTER JOIN userauth_customuser
+        ON weblog.member_no=userauth_customuser.id
+        WHERE userauth_customuser.sexType="WOMAN") A
+        RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+        GROUP BY hour
+        """)
+        rsWoman = cursor.fetchall()
+        # 남자
+        cursor.execute("""
+                with recursive time as
+                (select 0 as hour union all select hour+1 from time where hour<23)
+                SELECT TIME.hour, COUNT(distinct A.mno)
+                from(SELECT weblog.member_no mno, weblog.log_date log_date
+                FROM weblog LEFT OUTER JOIN userauth_customuser
+                ON weblog.member_no=userauth_customuser.id
+                WHERE userauth_customuser.sexType="MAN") A
+                RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+                GROUP BY hour
+                """)
+        rsMan = cursor.fetchall()
+
+    return render(request, "chart/chart2bySex.html", {
+        'rsWoman':rsWoman,
+        'rsMan':rsMan
+    })
+
+def chart2byAge(request):
+    import pymysql
+    dbCon = pymysql.connect(host='223.194.46.212',
+                            user='root',
+                            password='12345!',
+                            db='kocowa')
+    cursor = dbCon.cursor()
+
+    with dbCon:
+        # 10대
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct A.mno)
+        from(SELECT weblog.member_no mno, weblog.log_date log_date
+        FROM weblog LEFT OUTER JOIN userauth_customuser
+        ON weblog.member_no=userauth_customuser.id
+        WHERE userauth_customuser.age BETWEEN 10 AND 19) A
+        RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+        GROUP BY hour
+        """)
+        rs10 = cursor.fetchall()
+        # 20대
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct A.mno)
+        from(SELECT weblog.member_no mno, weblog.log_date log_date
+        FROM weblog LEFT OUTER JOIN userauth_customuser
+        ON weblog.member_no=userauth_customuser.id
+        WHERE userauth_customuser.age BETWEEN 20 AND 29) A
+        RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+        GROUP BY hour
+                """)
+        rs20 = cursor.fetchall()
+        # 30대
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct A.mno)
+        from(SELECT weblog.member_no mno, weblog.log_date log_date
+        FROM weblog LEFT OUTER JOIN userauth_customuser
+        ON weblog.member_no=userauth_customuser.id
+        WHERE userauth_customuser.age BETWEEN 30 AND 39) A
+        RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+        GROUP BY hour
+                """)
+        rs30 = cursor.fetchall()
+        # 40대
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct A.mno)
+        from(SELECT weblog.member_no mno, weblog.log_date log_date
+        FROM weblog LEFT OUTER JOIN userauth_customuser
+        ON weblog.member_no=userauth_customuser.id
+        WHERE userauth_customuser.age BETWEEN 40 AND 49) A
+        RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+        GROUP BY hour
+                """)
+        rs40 = cursor.fetchall()
+
+    return render(request, "chart/chart2byAge.html", {
+        'rs10':rs10,
+        'rs20':rs20,
+        'rs30': rs30,
+        'rs40': rs40
+    })
+
+def chart2byCity(request):
+    import pymysql
+    dbCon = pymysql.connect(host='223.194.46.212',
+                            user='root',
+                            password='12345!',
+                            db='kocowa')
+    cursor = dbCon.cursor()
+
+    with dbCon:
+        # 서울
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct A.mno)
+        from(SELECT weblog.member_no mno, weblog.log_date log_date
+        FROM weblog LEFT OUTER JOIN userauth_customuser
+        ON weblog.member_no=userauth_customuser.id
+        WHERE userauth_customuser.city = "Seoul" OR userauth_customuser.city = "서울") A
+        RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+        GROUP BY hour
+        """)
+        rsSeoul = cursor.fetchall()
+        # 대전
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct A.mno)
+        from(SELECT weblog.member_no mno, weblog.log_date log_date
+        FROM weblog LEFT OUTER JOIN userauth_customuser
+        ON weblog.member_no=userauth_customuser.id
+        WHERE userauth_customuser.city = "Daejeon" OR userauth_customuser.city = "대전") A
+        RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+        GROUP BY hour
+                """)
+        rsDaejeon = cursor.fetchall()
+        # 부산
+        cursor.execute("""
+        with recursive time as
+        (select 0 as hour union all select hour+1 from time where hour<23)
+        SELECT TIME.hour, COUNT(distinct A.mno)
+        from(SELECT weblog.member_no mno, weblog.log_date log_date
+        FROM weblog LEFT OUTER JOIN userauth_customuser
+        ON weblog.member_no=userauth_customuser.id
+        WHERE userauth_customuser.city = "Busan" OR userauth_customuser.city = "부산") A
+        RIGHT OUTER JOIN TIME ON (TIME.hour=HOUR(A.log_date))
+        GROUP BY hour
+                """)
+        rsBusan = cursor.fetchall()
+
+    return render(request, "chart/chart2byAge.html", {
+        'rsSeoul':rsSeoul,
+        'rsDaejeon':rsDaejeon,
+        'rsBusan': rsBusan,
+    })
