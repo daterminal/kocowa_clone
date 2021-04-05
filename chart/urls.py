55767a44239cfed55804adf77e05f7de4d8d6eab
lavamatchoco <38874351+lavamatchoco@users.noreply.github.com>
2021-04-06 00:52:11 +0900

    add membership chart
    add membership chart

--- chart/urls.py
+++ chart/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('chart2/', views.chart2, name="chart2"),
     path('chart2/bySex/', views.chart2bySex, name="chart2bySex"),
     path('chart2/byAge/', views.chart2byAge, name="chart2byAge"),
-    path('chart2/byCity/', views.chart2byCity, name="chart2byCity")
+    path('chart2/byCity/', views.chart2byCity, name="chart2byCity"),
+    path('chartM/', views.chartM, name="chartM"),
+    path('chartM/bySex/', views.chartM_Sex, name="chartM_Sex"),
+    path('chartM/byAge/', views.chartM_Age, name="chartM_Age"),
+    path('chartM/byCity/', views.chartM_City, name="chartM_City"),
 ]
